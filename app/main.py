c6f1e4e45f32aacb7c736fad2c91f5991940be34
today4king <today4king@gmail.com>
2021-05-18 23:15:05 +0800

    add full api: find-fist find-all contains-any replace

--- app/main.py
+++ app/main.py
@@ -21,14 +21,54 @@ search.SetKeywords(span_words)
 
 
 @app.post("/find-first")
-async def read_item(post: str):
+@app.get("/find-first")
+async def find_first(text: str):
     is_found = False
     import time
     start_t = time.time()
-    f = search.FindFirst(test_post)
+    f = search.FindFirst(text)
     end_t = time.time()
     time_ms = (end_t - start_t) * 1000
     if f:
         is_found = True
 
-    return {"is_found": is_found, "excuted_time": time_ms}
+    return {"is_found": is_found, "result": f, "excuted_time": time_ms}
+
+
+@app.post('find-all')
+@app.get('find-all')
+async def find_all(text: str):
+    is_found = False
+    import time
+    start_t = time.time()
+    fs = search.FindAll(text)
+    end_t = time.time()
+    time_ms = (end_t - start_t) * 1000
+    if fs and len(fs) > 0:
+        is_found = True
+
+    return {"is_found": is_found, "result": fs, "excuted_time": time_ms}
+
+
+@app.post('contains-any')
+@app.get('contains-any')
+async def contains_any(text: str):
+    import time
+    start_t = time.time()
+    b = search.ContainsAny(text)
+    end_t = time.time()
+    time_ms = (end_t - start_t) * 1000
+
+    return {"result": b, "excuted_time": time_ms}
+
+
+@app.post('replace')
+@app.get('replace')
+async def replace(text: str, replace_char: str = '*'):
+    import time
+    start_t = time.time()
+    text_replaced = search.Replace(text, replace_char)
+    end_t = time.time()
+    time_ms = (end_t - start_t) * 1000
+
+    return {"result": text_replaced, "excuted_time": time_ms}
